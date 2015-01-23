67d75e3f71f545931d53d73da7f20df2c7933b32
erlonlebon <erlonlebon@gmail.com>
2015-01-23 23:43:26 +0100

    C les antilles

--- public/js/game.js
+++ public/js/game.js
@@ -20,10 +20,8 @@ socket.on('chat:message', function(message) {
 
 $('a[href="harvest-wood"]').click(function(e) {
     e.preventDefault();
-    console.info('tu bois');
     socket.emit('harvest', 'wood');
 
     socket.on('gathering', function(data){
         console.info('ta bien bu', data);
-    })
-});
+    });
