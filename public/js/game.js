8487224d723d3a92c99a4555e3b3887d9f1afbd5
erlonlebon <erlonlebon@gmail.com>
2015-01-24 10:56:01 +0100

    buttons management

--- public/js/game.js
+++ public/js/game.js
@@ -18,28 +18,44 @@ socket.on('chat:message', function(message) {
     $(".chat").append('<p><strong>' + message.name + '</strong>: ' + message.message);
 });
 
+var disableButtons = function() {
+    $('.actions a').attr('disabled',true);
+};
+var enableButtons = function() {
+    $('.actions a').attr('disabled',false);
+};
+
 $('.actions a').click(function(e) {
     e.preventDefault();
+    disableButtons();
+    $(this).attr('disabled',false);
+    var inprogress = Ladda.create(this);
+    inprogress.start();
     switch(this.id) {
     	case 'harvest-wood':
 			socket.emit('harvest', 'wood');
-
 		    socket.on('gathering', function(data){
 		        console.info('ta bien bu', data);
+                inprogress.stop();
+                enableButtons();
 		    });
     	break;
     	case 'harvest-stone':
 			socket.emit('harvest', 'stone');
 
 		    socket.on('gathering', function(data){
+                inprogress.stop();
 		        console.info('Je ne vous jete pas la pierre', data);
+                enableButtons();
 		    });
     	break;
     	case 'hunt':
-			socket.emit('hunt', 'biche');
+			socket.emit('harvest', 'meat');
 
-		    socket.on('gathering', function(data){
+		    socket.on('hunting', function(data){
 		        console.info('Un sanglier', data);
+                inprogress.stop();
+                enableButtons();
 		    });
     	break;
     }
