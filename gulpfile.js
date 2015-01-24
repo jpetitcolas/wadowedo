f844dab9a3f7546ee41fd3e5353a232d2c470e4c
Emmanuel Quentin <manu.quentin@gmail.com>
2015-01-24 16:42:42 +0100

    Add gulfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+
+function buildScripts() {
+    gulp.src([
+        './public/js/spin.min.js',
+        './public/js/ladda.min.js',
+        './public/js/routing.js',
+        './public/js/game.js',
+        './public/js/player.js',
+        './public/js/player.js',
+        './public/js/buttons.js',
+        './public/js/buttons.js',
+        './public/js/chat.js',
+        './public/js/tribe.js'
+    ])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./public/dist/js/'))
+}
+
+
+gulp.task('default', function () {
+    'use strict';
+
+    buildScripts();
+
+    gulp.watch('public/js/**', buildScripts);
+});
+
+gulp.task('build', function () {
+    'use strict';
+
+    buildScripts();
+});
